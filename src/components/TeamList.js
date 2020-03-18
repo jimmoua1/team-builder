191c55bd0ed70bd1a8111beaa5a2b1a66a8fbd9f
jimmoua1 <tsheejmaus@yahoo.com>
2020-03-17 17:46:10 -0700

    TeamList.js added a return statement

--- src/components/TeamList.js
+++ src/components/TeamList.js
@@ -1,9 +1,27 @@
 import React, {} from 'react';
 
+import TemMemberDisplay from './TeamMemberDisplay';
+
 const TeamList = (props) => {
 
     return(
-
+        <main className="team-list">
+            <table>
+                <tbody>
+                    {
+                        props.teamMemberlist.map( (teamMember, i) => {
+                            return (
+                              <TeamMemberDisplay
+                                teamMember={teamMember}
+                                teamMemberList={props.teamMemberList}
+                                key={"team-" + i.toString().padStart(2, "0")}
+                              />
+                            );
+                        })
+                    }
+                </tbody>
+            </table>
+        </main>
     )
 }
 
